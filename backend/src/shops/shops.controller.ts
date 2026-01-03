aa4c8fc598b988bbd17c341ec4d05ee9537f833e
csakos1 <akos@csaba.vip>
2026-01-03 19:50:07 +0100

    Látogatottság mérés hozzáadása

--- backend/src/shops/shops.controller.ts
+++ backend/src/shops/shops.controller.ts
@@ -7,7 +7,9 @@ import {
   Param,
   Delete,
   UseGuards,
+  Req,
 } from '@nestjs/common';
+
 import { ShopsService } from './shops.service';
 import { CreateShopDto } from './dto/create-shop.dto';
 import { UpdateShopDto } from './dto/update-shop.dto';
@@ -40,7 +42,11 @@ export class ShopsController {
   // --- PUBLIKUS VÉGPONTOK (Bárki láthatja) ---
 
   @Get()
-  findAll() {
+  findAll(@Req() request: any) {
+    // Itt kérjük be a Request-et
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const ip = request.socket.remoteAddress;
+    console.log(`[${new Date().toISOString()}] App megnyitva innen: ${ip}`);
     return this.shopsService.findAll();
   }
 
